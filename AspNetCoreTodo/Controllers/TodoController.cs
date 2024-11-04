b5484d82a77cb6190f875fde9c084fbc43bec1de
Mathew Keeling <soterialogos@protonmail.com>
2024-11-03 21:34:05 -0500

    Header Modifications

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Mvc;
-using AspNetCoreTodo.Services;
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Controllers
 {
@@ -17,16 +17,16 @@ namespace AspNetCoreTodo.Controllers
             _todoItemService = todoItemService;
         }
 
-        public async IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var items  = await _todoItemsService.GetIncompleteItemsAsync();
-            
-            // Get to-do items from database
+            var items = await _todoItemService.GetIncompleteItemsAsync();
 
-            // Put items into a model
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
 
-            // Pass the view to a model and render
+            return View(model);
         }
     }
 }
-
